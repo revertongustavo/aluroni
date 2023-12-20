f0a2edfb880eda94529d8239657305f774808dac
Reverton Silva <revertong@gmail.com>
2023-12-20 16:57:48 -0700

    Rendezirando as imagens dos itens

--- src/Pages/Cardapio/Itens/index.tsx
+++ src/Pages/Cardapio/Itens/index.tsx
@@ -1,12 +1,12 @@
-import cardapio from './itens.json'; 
 import Item from './Item';
 import styles from './Itens.module.scss';
+import cardapio from './itens.json'; 
 
 export default function Itens() {
   return (
     <div className={styles.itens}>
       {cardapio.map(item => (
-        <Item key={item.id} />
+        <Item key={item.id} {...item} />
       ))}
     </div>
   )
